e6e834ed50636730f8e4909d78b651241465ee58
lmbernardo7520112 <lmbernardo752011@gmail.com>
2021-03-10 19:25:45 -0300

    Admin sigin and signout aula 6.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, skip: [:registrations], controllers: {sessions: 'admins/sessions'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    #admin/posts
+    #root to: "admin/admin/home#index" (dá errado)
+    #root to: "admin/home#index" (dá errado mas é o que o prof escreve na aula)
+    root to: "home#index" #só tá dando certo c esse)
+  end
 end
