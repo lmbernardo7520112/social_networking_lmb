0797e5e760b49051078bca80a17be66856fb29ba
lmbernardo7520112 <lmbernardo752011@gmail.com>
2021-03-11 22:55:29 -0300

    Crud administradores parte 5 at 04:34.

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -3,8 +3,35 @@ class Admin::AdminsController < AdminController
     @admins = Admin.order(id: :desc)
    end 
 
+
    def new
     @admin = Admin.new
+   end
+
+
+   def create
+    @admin = Admin.new(form_params)
+    if @admin.save
+        redirect_to admin_admins_path
+    else
+        render :new
+    end
+   end
+
+   def edit
+    @admin = Admin.find(params[:id])
+   end
+
+   def update
+    @admin = Admin.find(params[:id])
+    params = form_params.to_h
+    params = params.except!(:password, :password_confirmation) if params[:pasword].blank?
+
+    if @admin.update(params)
+        redirect_to admin_admins_path
+    else
+        render :edit
+    end
    end
 
    private
@@ -12,4 +39,5 @@ class Admin::AdminsController < AdminController
    def form_params
     params.require(:admin).permit(:name, :email, :password, :password_confirmation)
    end
+
 end
